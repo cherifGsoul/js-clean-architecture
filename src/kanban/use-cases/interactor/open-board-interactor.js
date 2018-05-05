550b23cf9483cc6c1a8f225b7fb0ab659e58b853
cherif BOUCHELAGHEM <cherifbouchelaghem@yahoo.fr>
2018-05-05 02:33:16 +0100

    reorganise project structure

--- src/kanban/use-cases/interactor/open-board-interactor.js
+++
@@ -1,9 +0,0 @@
-const makeBoard = require('../../domain/model/board/board');
-
-module.exports = (boardGateway) => {
-  return ({title}) => {
-    const board = makeBoard(boardGateway.nextIdentity(), title);
-    boardGateway.save(board);
-    return board;
-  }
-}
--- src/kanban/use-cases/request/open-board-request.js
+++
@@ -1,5 +0,0 @@
-module.exports = (title) => {
-  return {
-    title
-  }
-}
